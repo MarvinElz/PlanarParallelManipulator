c6e17861679a767d93c4029764f7923ecc08c15c
Marvin Elz <MarvinElz@gmx.de>
2016-07-15 06:36:33 +0200

    slightly changed shed_mem.h

--- shrd_mem.h
+++ shrd_mem.h
@@ -8,11 +8,11 @@
 struct shared_mem_struct{
 	long phi_b;			// Position Gelenk 1
 	long phi_c;			// Position Gelenk 2
-    long phi_b_soll;	// SollPosition Gelenk 1
-    long phi_b_soll;	// SollPosition Gelenk 2
+	long phi_b_soll;	// SollPosition Gelenk 1
+	long phi_b_soll;	// SollPosition Gelenk 2
 }typedef shared_mem_struct;
 
-int shared_mem_id;						// shared-memory key
+int shared_mem_id;					// shared-memory key
 shared_mem_struct *shared_mem;		// Pointer on shared_mem_struct
 
 #define SMkey 58120526
