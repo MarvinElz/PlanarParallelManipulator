768c114864320be3b6f4222a68c6534bb5054669
MarvinElz <MarvinElz@gmx.de>
2016-07-16 16:32:45 +0200

    2. Encoder hinzugefügt
    encoder.c fragt beide encoder ab und schreibt deren wert in den shared
    memory

--- encoder.c
+++ encoder.c
@@ -13,19 +13,26 @@ struct Zustand{
 	char ai[4];
 } typedef Zustand;
 
-Zustand Automat[4] = {  {  1, { 0, 1, 2, 3 } },   // rechtsdrehung
-						{  0, { 0, 1, 2, 3 } },   // linksdrehung
-						{  1, { 1, 0, 3, 2 } },   // rechtsdrehung
-						{  0, { 1, 0, 3, 2 } } }; // linksdrehung
+Zustand Automat[4] = {  
+        {  1, { 0, 1, 2, 3 } },   // rechtsdrehung
+				{  0, { 0, 1, 2, 3 } },   // linksdrehung
+				{  1, { 1, 0, 3, 2 } },   // rechtsdrehung
+				{  0, { 1, 0, 3, 2 } } }; // linksdrehung
 
-char state = 0;
-char eingabe;		// interpretiertes Einlesen [0..3]
-char alteeingabe;   // wird benötigt für Haltepositionen, Ausgabe nur bei Änderung
+char Enc1_state = 0;
+char Enc1_input;		// interpretiertes Einlesen [0..3]
+char Enc1_input_old;   // wird benötigt für Haltepositionen, Ausgabe nur bei Änderung
+
+char Enc2_state = 0;
+char Enc2_input;    // interpretiertes Einlesen [0..3]
+char Enc2_input_old;   // wird benötigt für Haltepositionen, Ausgabe nur bei Änderung
 
 
 int init(){
 	pinMode ( enc1_a, INPUT );
 	pinMode ( enc1_b, INPUT );
+  pinMode ( enc2_a, INPUT );
+  pinMode ( enc2_b, INPUT );
 }
 
 int main( int argc, const char* argv[] ){
@@ -35,18 +42,34 @@ int main( int argc, const char* argv[] ){
 
    while(1){
 
-       eingabe = digitalRead( enc1_a ) << 1 | digitalRead( enc1_b );
-       
-   	  if (eingabe != alteeingabe){    // wenn Änderung stattgefunden hat
-         alteeingabe = eingabe;       //      
-         state = Automat[AktuellerZustand].ai[eingabe]; // neuer Zustand wird aus dem Automaten ermittelt      
+      Enc1_input = digitalRead( enc1_a ) << 1 | digitalRead( enc1_b );       
+      if (Enc1_input != Enc1_input_old){    // wenn Änderung stattgefunden hat
+         Enc1_input_old = Enc1_input;       // Eingabe speichern für Abgleich    
+         Enc1_state = Automat[Enc1_state].ai[Enc1_input]; // neuer Zustand wird aus dem Automaten ermittelt      
           
-         shared_mem = (shared_mem_struct*)shmat(shID, 0, 0);
-         if( Automat[state].outValue )
+         shared_mem = (shared_mem_struct*)shmat(shID, 0, 0);  // Zugriff auf shared memory bekommen
+         if( Automat[Enc1_state].outValue )
             shared_mem->phi_b++;
          else
             shared_mem->phi_b--;
          shmdt(shared_mem);
       }
+
+
+      Enc2_input = digitalRead( enc2_a ) << 1 | digitalRead( enc2_b );       
+      if (Enc2_input != Enc2_input_old){    // wenn Änderung stattgefunden hat
+         Enc2_input_old = Enc2_input;       // Eingabe speichern für Abgleich    
+         Enc2_state = Automat[Enc2_state].ai[Enc2_input]; // neuer Zustand wird aus dem Automaten ermittelt      
+          
+         shared_mem = (shared_mem_struct*)shmat(shID, 0, 0);  // Zugriff auf shared memory bekommen
+         if( Automat[Enc2_state].outValue )
+            shared_mem->phi_c++;
+         else
+            shared_mem->phi_c--;
+         shmdt(shared_mem);
+      }
+      
+
+
    }
 }
